d99cc06b8c89d0fb99336ae1d53789b161b81643
GayathriRajendran17 <92028340+GayathriRajendran17@users.noreply.github.com>
2023-03-19 20:03:16 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require("express");
 var app = (module.exports = express());
 
 app.get("/", function (req, res) {
-  res.send("Hello Gayathri!");
+  res.send("Hello Gaya3!");
 });
 
 if (!module.parent) {
